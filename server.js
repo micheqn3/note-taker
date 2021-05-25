40f6b9468eacca0adad6163e318c0e0b9f253133
Michelle Nguyen <michellenguyen11239@aol.com>
2021-05-25 16:12:36 -0700

    Updated server to send db as response

--- server.js
+++ server.js
@@ -6,9 +6,7 @@ const uniqid = require('uniqid'); // Generates random ID number for notes
 const port =  process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true })); // Sets up data parsing
 app.use(express.json());
-app.use(express.static('public')) // Serves static in public folder
-app.use(express.static('db')) // Serves static files in db folder
-
+app.use(express.static('public')) // Serves static files in public folder
 
 app.get('/', (req, res) => { // Route to index.html
     res.sendFile(__dirname + '/public' + '/index.html')
@@ -20,6 +18,7 @@ app.get('/notes', (req, res) => { // Route to notes.html
 app.get('/api/notes', (req, res) => { // Array of objects of notes
     //res.send(allNotes);
     // res.send([{"title": "hello", "text": "here is my text", "id": 322}]); // testing this right now
+    /*
     fs.readFile('./db/db.json', 'utf8', (error, data) => {
         if (error) {
             console.log("Something went wrong.")
@@ -27,6 +26,8 @@ app.get('/api/notes', (req, res) => { // Array of objects of notes
             res.send(data); // send JSON file data
         }
     })
+    */
+   res.sendFile(__dirname + '/db' + '/db.json');
 })
 
 app.post('/api/notes', (req, res) => { // Handles post request for notes
