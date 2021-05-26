850ade101c139eb4949b55ede43d32f56ab85758
Michelle Nguyen <michellenguyen11239@aol.com>
2021-05-26 12:08:24 -0700

    Updated styling and posting to database

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port =  process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true })); // Sets up data parsing
 app.use(express.json());
 app.use(express.static('public')) // Serves static files in public folder
-const db = require('./db/db.json') // Importing database 
+app.use(express.static('db')) 
 
 app.get('/', (req, res) => { // Route to index.html
     res.sendFile(__dirname + '/public' + '/index.html')
@@ -15,21 +15,28 @@ app.get('/notes', (req, res) => { // Route to notes.html
     res.sendFile(__dirname + '/public' + '/notes.html')
 })
 
-app.get('/api/notes', (req, res) => { // Array of objects of notes
+app.get('/api/notes', (req, res) => { // Db of notes
    res.sendFile(__dirname + '/db' + '/db.json');
 })
 
 app.post('/api/notes', (req, res) => { // Handles post request for notes
     let obj = req.body;
-    obj.id = uniqid(); 
-    db.push(obj); 
-    fs.writeFile(__dirname + '/db' + '/db.json', JSON.stringify(db), (error) => { // Update the database with the note
+    obj.id = uniqid(); // Generate random ID
+    fs.readFile(__dirname + '/db' +'/db.json', 'utf-8', (error, data) => {
         if(error) {
             throw error;
         } else {
-            console.log("Saved to database.")
+            let d = JSON.parse(data);
+           d.push(obj);
+           fs.writeFile(__dirname + '/db' + '/db.json', JSON.stringify(d), (error) => { // Update the database with the note
+            if(error) {
+                throw error;
+            } else {
+                console.log("Saved to database.")
+            }
+        })  
         }
-    })    
+    })
 })
 
 app.delete('/api/notes/:id', (req, res) => { // Handles deleting notes
@@ -40,7 +47,7 @@ app.delete('/api/notes/:id', (req, res) => { // Handles deleting notes
             console.log("Something went wrong.")
         } else {
             let parsedData = JSON.parse(data);
-            for (let i = 0 ; i < parsedData.length ; i++) {
+            for (let i = 0 ; i < parsedData.length ; i++) { // Remove the array with the matching ID
                 if(parsedData[i].id === id) {
                     parsedData.splice(i, 1);
                 }
@@ -53,23 +60,6 @@ app.delete('/api/notes/:id', (req, res) => { // Handles deleting notes
                     }
                 })
             }
-            /*
-            let parsedData = JSON.parse(data);
-            let newArray = [];
-            for (let i = 0 ; i < parsedData.length ; i++) { // If the object's id is not equal to the parameter requested, save to new array
-                if(parsedData[i].id !== id) { 
-                    newArray.push(parsedData[i]);
-                }
-            }
-            
-            fs.writeFile(__dirname + '/db' + '/db.json', JSON.stringify(newArray), (error) => { // Write new array to database
-                if(error) {
-                    throw error;
-                } else {
-                    console.log("Deleted item.");
-                }
-            })
-            */
         }
     })
 })
